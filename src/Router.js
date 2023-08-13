d5bff5fd3f31efca9a923dba50cca6f01c72aa1c
MihyeJeon <jeonmye@gmail.com>
2023-08-13 21:40:30 +0900

    style : 최상단 div common css작성 / feat : Route컴포넌트에 PostList 컴포넌트 연결

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import PostList from './pages/PostList/PostList';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/post-list" element={<PostList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PostList/PostList.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+
+const PostList = () => {
+  return <div className="PostList"></div>;
+};
+
+export default PostList;
