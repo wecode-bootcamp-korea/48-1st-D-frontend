2ce1e1f4729dc6ae427c38b1a63cd5288ae3e334
serenity1091 <seulki1091@gmail.com>
2023-08-14 10:44:39 +0900

    PostEditing Layout 구현, Common.scss 수정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import PostEditing from './pages/PostEditing/PostEditing';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/postediting" element={<PostEditing />} />
       </Routes>
     </BrowserRouter>
   );
