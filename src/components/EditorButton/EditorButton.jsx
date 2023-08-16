75be6cb20372ddbd5ed60eaa400333e84bda8ab5
MihyeJeon <jeonmye@gmail.com>
2023-08-17 03:11:11 +0900

    feat : 글쓰기 버튼 클릭시 페이지 이동 구현

--- src/components/EditorButton/EditorButton.jsx
+++ src/components/EditorButton/EditorButton.jsx
@@ -1,9 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import './EditorButton.scss';
 
+{
+  /*TODO 로그인 여부에 따른 글쓰기 버튼 활성화/비활성화*/
+}
 const EditorButton = () => {
+  const navigate = useNavigate();
+
+  const EditorButtonOnClick = () => {
+    navigate('/post-add');
+  };
   return (
     <div className="editorBox">
-      <button className="editorButton">글 쓰기</button>;
+      <button className="editorButton" onClick={EditorButtonOnClick}>
+        글 쓰기
+      </button>
+      ;
     </div>
   );
 };
