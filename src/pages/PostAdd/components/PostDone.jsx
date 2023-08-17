39616f5e0aa41ce3056900c5ea68ca371f359d94
serenity1091 <seulki1091@gmail.com>
2023-08-17 10:23:55 +0900

    post-add, post-editing 페이지 기능구현 완료 / 버튼 컴포넌트 재사용

--- src/pages/PostAdd/components/PostDone.jsx
+++ src/pages/PostAdd/components/PostDone.jsx
@@ -10,15 +10,17 @@ const PostDone = () => {
   };
 
   return (
-    <div className="modalPostDone">
+    <div className="rootModal">
       <h1>포스트 게시 완료 페이지</h1>
-      <div className="modal">
-        <img className="modalImg" src="images/Check.png" alt="게시 체크" />
-        <div className="modalPost">
-          <p className="modalContent">게시되었습니다.</p>
-          <button className="goToPostList" onClick={goToPostList}>
-            보기
-          </button>
+      <div className="modalPostDone">
+        <div className="modal">
+          <img className="modalImg" src="images/Check.png" alt="게시 체크" />
+          <div className="modalPost">
+            <p className="modalContent">게시되었습니다.</p>
+            <button className="goToPostList" onClick={goToPostList}>
+              보기
+            </button>
+          </div>
         </div>
       </div>
     </div>
